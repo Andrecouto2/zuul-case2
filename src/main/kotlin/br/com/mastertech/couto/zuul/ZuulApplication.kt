df40cbb6207cbf329224f92674765bd6359f43e3
andrecouto <coutoandreluis@gmail.com>
2020-08-17 15:53:08 -0300

    zuul

---
+++ src/main/kotlin/br/com/mastertech/couto/zuul/ZuulApplication.kt
@@ -0,0 +1,13 @@
+package br.com.mastertech.couto.zuul
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy
+
+@SpringBootApplication
+@EnableZuulProxy
+class ZuulApplication
+
+fun main(args: Array<String>) {
+    runApplication<ZuulApplication>(*args)
+}
